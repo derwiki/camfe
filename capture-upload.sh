10d369b06a40124c2dbe144886d4bc3acf464a39
Adam Derewecki <derewecki@gmail.com>
2019-10-01 17:36:49 -0700

    Add CAMBE_HOST

--- capture-upload.sh
+++ capture-upload.sh
@@ -2,8 +2,9 @@
 
 user=$CAMBE_USER
 pass=$CAMBE_PASS
+host=$CAMBE_HOST
 
-raspistill -vf -hf -o /tmp/capt0000.jpg
-echo captured
-curl -F "image=@/tmp/capt0000.jpg" https://$user:$pass@cambe.herokuapp.com/upload
-echo uploaded
+raspistill -vf -hf -o /tmp/capt0000.jpg && echo captured
+curl -F "host=$CAMBE_HOST" \
+     -F "image=@/tmp/capt0000.jpg" \
+     https://$user:$pass@cambe.herokuapp.com/upload && echo uploaded
