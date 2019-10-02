6c7d56d6aa27557f3535170e7e7bc94e54f42818
Adam Derewecki <derewecki@gmail.com>
2019-10-01 17:02:45 -0700

    g add main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user=$CAMBE_USER
+pass=$CAMBE_PASS
+
+while true
+do
+  raspistill -a -q 100 -vf -hf -o /tmp/capt0000.jpg
+  curl -F "image=@/tmp/capt0000.jpg" https://$user:$pass@cambe.herokuapp.com/upload
+  sleep 1
+done
