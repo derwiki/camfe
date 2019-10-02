c33c0501badb29925e8b271de1c55fd5d367eb04
Adam Derewecki <derewecki@gmail.com>
2019-10-01 17:04:18 -0700

    fix -q 100

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ pass=$CAMBE_PASS
 
 while true
 do
-  raspistill -a -q 100 -vf -hf -o /tmp/capt0000.jpg
+  raspistill -a -q100 -vf -hf -o /tmp/capt0000.jpg
   curl -F "image=@/tmp/capt0000.jpg" https://$user:$pass@cambe.herokuapp.com/upload
   sleep 1
 done
