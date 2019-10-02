49e0e788e6f56acbad1d9c518fb2c0165699faf6
Adam Derewecki <derewecki@gmail.com>
2019-10-01 17:52:13 -0700

    Add a space

--- capture-upload.sh
+++ capture-upload.sh
@@ -4,7 +4,7 @@ user=$CAMBE_USER
 pass=$CAMBE_PASS
 host=$CAMBE_HOST
 
-raspistill -q 7 -vf -hf -o /tmp/capt0000.jpg && echo captured
+raspistill -q 7 -vf -hf -o /tmp/capt0000.jpg && echo " captured"
 curl -F "host=$CAMBE_HOST" \
      -F "image=@/tmp/capt0000.jpg" \
-     https://$user:$pass@cambe.herokuapp.com/upload && echo uploaded
+     https://$user:$pass@cambe.herokuapp.com/upload && echo " uploaded"
